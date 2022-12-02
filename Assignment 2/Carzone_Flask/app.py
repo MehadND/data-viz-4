20bbff4f82b055eba68ac944effce63e684e4c4a
MehadND <mehadnd@gmail.com>
2022-12-02 15:26:32 +0000

    added another graph

--- Assignment 2/Carzone_Flask/app.py
+++ Assignment 2/Carzone_Flask/app.py
@@ -65,6 +65,34 @@ def viewtable():
 
 @app.route('/viewgraph', methods=['GET', 'POST'])
 def viewgraph():
+    # try:
+    #     cars = Car.query.order_by(Car.id).all() 
+    #     for car in cars:
+    #         car.content = ImmutableMultiDict(json.loads(car.content))
+    # except:
+    #     pass
+    
+    # cars_content = []
+    # for car in cars:
+    #     car.content = dict(car.content)
+    #     cars_content += [car.content]
+    
+    # df = pd.DataFrame(cars_content)
+    
+    # df.sort_values("Price", inplace=True)
+    
+    # fig = px.scatter(df, x='Year', y='Price', color='Make')
+    # fig.update_xaxes(range=[1990, 2024])
+    # fig['layout']['yaxis'].update(autorange = True)
+    # fig.show()
+    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # return render_template('viewgraph.html', graphJSON = graphJSON)
+    return render_template('viewgraph.html')
+
+
+@app.route('/graphone', methods=['GET', 'POST'])
+def graphone():
     try:
         cars = Car.query.order_by(Car.id).all() 
         for car in cars:
@@ -90,6 +118,39 @@ def viewgraph():
     return render_template('viewgraph.html', graphJSON = graphJSON)
 
 
+@app.route('/graphtwo', methods=['GET', 'POST'])
+def graphtwo():
+    try:
+        cars = Car.query.order_by(Car.id).all() 
+        for car in cars:
+            car.content = ImmutableMultiDict(json.loads(car.content))
+    except:
+        pass
+    
+    cars_content = []
+    for car in cars:
+        car.content = dict(car.content)
+        cars_content += [car.content]
+    
+    df = pd.DataFrame(cars_content)
+    
+    data = dict(
+        character=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+        parent=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+        value=[10, 14, 12, 10, 2, 6, 6, 4, 4])
+
+    fig = px.sunburst(
+        df,
+        names='County',
+        parents='Make',
+        values='Price',
+    )
+    fig.show()
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    return render_template('viewgraph.html', graphJSON = graphJSON)
+
+
 @app.route('/uploadcsv', methods=['GET', 'POST'])
 def uploadcsv():
 
@@ -97,7 +158,7 @@ def uploadcsv():
         f = request.files['File']
         df = pd.read_csv(f)
         for index, row in df.iterrows():
-            car_dict = {k: row[k] for k in ['Name', 'Year', 'Price', 'Make', 'Model', 'Engine', 'Fuel', 'Odometer', 'Transmission', 'Body', 'Year', 'County', 'Doors', 'Color', 'Owners', 'Tax-Expiry', 'NCT-Expiry']}
+            car_dict = {k: row[k] for k in ['Name', 'Year', 'Price', 'Make', 'Model', 'Engine', 'Fuel', 'Odometer', 'Transmission', 'Body', 'County', 'Doors', 'Color', 'Owners', 'Tax-Expiry', 'NCT-Expiry']}
             car_json = json.dumps(car_dict)
             new_car = Car(content=car_json)
             db_err = foo.db_add_one(new_car, db)
